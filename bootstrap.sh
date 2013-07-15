3732efd844360f6673f2be5b5acf93c88a34255c
Henning Schroder <henning.schroeder@gmail.com>
2013-07-15 22:28:03 +0200

    fixes

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,13 +1,44 @@
 #!/bin/bash
-set -e -x
+set -e 
 
-DIST=/opt/anaconda
-TARGET=/tmp/freeconda
+DIST=$1
+TARGET=$2
+
+function usage() {
+  echo "Usage: $0 FROM_DISTRIBUTION TARGET"
+  exit 1
+}
+
+if [ "$DIST" = "" ]
+then
+  usage
+else
+  for tool in python conda
+  do
+    if [ -f "$DIST/bin/$tool" ]
+    then
+       echo "Found $DIST/bin/$tool"
+    else
+       echo "Error: Could not find $DIST/bin/$tool"
+       exit 2
+    fi
+  done
+fi
+if [ "$TARGET" = "" ]
+then
+  usage
+else
+  if [ -d $TARGET ]
+  then
+    echo "Warning: Target $TARGET already exists"
+  fi
+fi
+
 PKGDIR=$DIST/conda-bld/linux-64/
 PYTHON=$DIST/bin/python
 CONDA="$PYTHON $DIST/bin/conda"
 
-for package in system zlib bzip2 openssl readline sqlite python pycosat conda setuptools
+for package in system zlib bzip2 openssl readline sqlite python pycosat pyyaml conda setuptools
 do
   test -f $PKGDIR/$package*.tar.bz2 || $CONDA build $package
 done
@@ -17,6 +48,7 @@ for pkgfile in $PKGDIR/*.tar.bz2
 do
   tar -xjv -C /tmp/freeconda -f $pkgfile
 done
+
 
 cp $PKGDIR/*.tar.bz2 $TARGET/pkgs/
 

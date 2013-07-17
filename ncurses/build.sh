03b2e60e8244f5f72dc340f80c66295b6df5bebd
Henning Schroder <henning.schroeder@gmail.com>
2013-07-17 14:58:00 +0200

    typo

--- ncurses/build.sh
+++ ncurses/build.sh
@@ -1,4 +1,7 @@
 # http://www.linuxfromscratch.org/lfs/view/development/chapter06/ncurses.html
-./configure --prefix=$PREFIX -with-shared      --without-debug  --enable-pc-files  #--enable-widec
+./configure --prefix=$PREFIX \
+ --with-shared      \
+ --without-debug  \
+ --enable-pc-files  #--enable-widec
 make
 make install

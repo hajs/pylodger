cfb922c52ae6330dd93b5f9982393930cafad7d0
Henning Schroder <henning.schroeder@gmail.com>
2013-07-18 18:01:54 +0200

    fix building atlas

--- recipes/sci/atlas/build.sh
+++ recipes/sci/atlas/build.sh
@@ -1,6 +1,6 @@
 mkdir build
 cd build
-../configure --with-netlib-lapack-tarfile=$SYS_PREFIX/conda-bld/src_cache/lapack.tgz --shared
+../configure --with-netlib-lapack-tarfile=$SYS_PREFIX/conda-bld/src_cache/lapack.tgz --shared y -Fa alg '-fPIC'
 make 
 cd lib
 mkdir -p $PREFIX/lib

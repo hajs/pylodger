c0ae95c587b502f235ed3031fbf1cae3bbe091a2
Henning Schroder <henning.schroeder@gmail.com>
2013-07-29 13:54:15 +0200

    build fixes for pygrib

--- recipes/sci/gribapi/build.sh
+++ recipes/sci/gribapi/build.sh
@@ -4,12 +4,12 @@ export CFLAGS="-O2 -fPIC -I$PREFIX/include/openjpeg-1.5"
 
 export GRIBAPI_DIR=$PREFIX
 export JASPER_DIR=$PREFIX
-#export OPENJPEG_DIR=$PREFIX
+export OPENJPEG_DIR=$PREFIX
 export PNG_DIR=$PREFIX
 
 
 #./configure --prefix=$PREFIX --enable-python --with-openjpeg=$OPENJPEG_DIR 
-./configure --prefix=$PREFIX --enable-python --with-jasper=$JASPER_DIR
+./configure --prefix=$PREFIX --enable-python --with-jasper=$JASPER_DIR --enable-shared
 #./configure --prefix=$PREFIX --enable-python --disable-jpeg
 make
 #make check
--- recipes/sci/pygrib/build.sh
+++ recipes/sci/pygrib/build.sh
@@ -1,8 +1,9 @@
 export CFLAGS="-O2 -fPIC"
-
+export CPPFLAGS="-O2 -fPIC"
 export GRIBAPI_DIR=$PREFIX
 export JASPER_DIR=$PREFIX
 export OPENJPEG_DIR=$PREFIX
 export PNG_DIR=$PREFIX
 
+$PYTHON setup.py build_ext --rpath=$PREFIX
 $PYTHON setup.py install --prefix=$PREFIX

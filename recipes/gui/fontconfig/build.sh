6be8316c74a4fe204f0edcfdf766d4c6355fa317
Henning Schroder <henning.schroeder@gmail.com>
2013-07-29 14:58:00 +0200

    fixed linking to freetype and libxml2

--- recipes/gui/fontconfig/build.sh
+++ recipes/gui/fontconfig/build.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 chmod +x configure
-export FREETYPE_LIBS=-L$PREFIX/lib/
+export FREETYPE_LIBS="-L$PREFIX/lib/ -lfreetype"
 export FREETYPE_CFLAGS=-I$PREFIX/include/freetype2
 export LIBS=-L$PREFIX/lib/
 export CFLAGS=-I$PREFIX/include
 export LIBXML2_CFLAGS=-I$PREFIX/include/libxml2
-export LIBXML2_LIBS=-L$PREFIX/lib/
+export LIBXML2_LIBS="-L$PREFIX/lib/ -lxml2"
 ./configure --prefix $PREFIX --enable-libxml2
+bash -i
 make
-make instal
+make install

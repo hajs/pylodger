998ae577de329b134712e7dfa02b279ecc4d2b27
Henning Schroder <henning.schroeder@gmail.com>
2013-07-29 22:33:16 +0200

    fixed deps

--- recipes/gui/pyqt/build.sh
+++ recipes/gui/pyqt/build.sh
@@ -1,4 +1,4 @@
-export PATH=$SYS_PREFIX/bin:$PATH  # to find qmake
+export QMAKESPEC=linux-g++
 python configure.py --bindir $PREFIX/bin/ --destdir $PREFIX/lib/python2.7/site-packages/ --no-designer-plugin --sipdir $PREFIX/share/sip/PyQt4 --no-qsci-api --confirm-license
 make
 make install
--- recipes/gui/shiboken/build.sh
+++ recipes/gui/shiboken/build.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
+# http://around-the-corner.typepad.com/adn/2012/10/building-qt-pyqt-pyside-for-maya-2013-part-2.html
 
 mkdir build
 cd build
-cmake \
+/usr/bin/cmake \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_INSTALL_RPATH=$LD_RUN_PATH \
     -DQT_QMAKE_EXECUTABLE=$PREFIX/bin/qmake \
     -DPYTHON_EXECUTABLE=$PYTHON \
     -DLIB_INSTALL_DIR=$PREFIX/lib \
+    -DPYTHON_INCLUDE_PATH=$PREFIX/include/python2.7 \
+    -DPYTHON_LIBRARY=$PREFIX/lib/libpython2.7.so \        
     ..
 make VERBOSE=2
 make install

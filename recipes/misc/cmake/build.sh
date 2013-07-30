ca4d8430252c416c2d57918dfaf3c519d84b0e07
Henning Schroder <henning.schroeder@gmail.com>
2013-07-30 16:53:25 +0200

    added dependencies

--- recipes/misc/cmake/build.sh
+++ recipes/misc/cmake/build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# http://www.cmake.org/pipermail/cmake/2008-January/019290.html
+#export LDFLAGS='-Wl,-rpath=$ORIGIN/../lib'
 CC=cc CXX=c++ ./configure --prefix=$PREFIX
 make
 rm $SRC_DIR/Modules/CPack.OSXScriptLauncher.in

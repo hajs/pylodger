855adea623e720eaec046387c0df32d90051ed72
Henning Schroder <henning.schroeder@gmail.com>
2013-08-05 15:35:47 +0200

    added spyder ide

--- recipes/base/pip/build.sh
+++ recipes/base/pip/build.sh
@@ -2,3 +2,8 @@
 $PYTHON setup.py install
 #wget https://raw.github.com/pypa/pip/master/contrib/get-pip.py
 #$PYTHON get-pip.py
+mkdir -p $PREFIX/bin
+echo "#!/bin/sh
+python -m pip $*
+" > $PREFIX/bin/pip
+chmod +x $PREFIX/bin/pip

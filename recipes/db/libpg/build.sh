d585034e43baa39e06c7940507f7c9a8749a6da5
Henning Schroder <henning.schroeder@gmail.com>
2013-08-05 13:12:09 +0200

    build all but only install libpg, pg-config and includes

--- recipes/db/libpg/build.sh
+++ recipes/db/libpg/build.sh
@@ -1,5 +1,13 @@
-./configure --prefix=$PREFIX
-cd src/interfaces/libpq
+./configure --prefix=$PREFIX --without-readline
 make
+
+cd src/interfaces/libpq
 make install
 
+cd ../..
+cd bin/pg_config
+make install
+
+cd ../../
+cd include
+make install
